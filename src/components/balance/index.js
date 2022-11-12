ca0ca9307c0a211e887687504c8cd12cc0b8315a
mateus nascimento <mateus.nasciment@hotmail.com>
2022-11-12 14:40:01 -0300

    balance complet

--- src/components/balance/index.js
+++ src/components/balance/index.js
@@ -10,7 +10,7 @@ return (
     <View style={styles.container}>
 
         <view style={styles.item}>
-            <Text style={styles.itemSaldo}>Saldo</Text>
+                <Text style={styles.itemSaldo}>Saldo</Text>
             <view style={styles.content}>
                     <Text style={styles.currencySymbol}>R$</Text>
                     <Text style={styles.balance}>{saldo}</Text>
@@ -19,7 +19,7 @@ return (
 
 
         <view style={styles.item}>
-            <Text style={styles.itemGasto}>Gastos</Text>
+            <Text style={styles.itemGasto}>Gasto</Text>
             <view style={styles.content}>
                     <Text style={styles.currencySymbol}>R$</Text>
                     <Text style={styles.balance}>{gastos}</Text>
@@ -29,48 +29,51 @@ return (
     </View>
     );
 }
-/* A style sheet. */
+
 const styles =StyleSheet.create({
-    container: {
-        backgroundColor: '#e6e8e3',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingStart: 18,
-        paddingTOP: 20,
-        paddingEnd: 28,
-        marginTop: -24,
-        marginStart: 8,
-        marginEnd: 8,
-        borderRadius: 8,
-        paddingBottom:  40,
-        zIndex: 99,
-    },
-    itemGasto: {
-        color: '#ff0000',
-        fontSize: 26,
-        fontFamily: 'Poppins_400Regular',
-    },
-    itemSaldo: {
-        color: '#00ff00',
-        fontSize: 26,
-        fontFamily: 'Poppins_400Regular',
-    },
-    content: {
-        flexDirection: 'column',
-        aligntItems: 'end',
-    },
-    currencySymbol: {
-        color: '#000000',
-        marginRight: 6,
-        marginStart: 6,
-    },
-    balance: {
-        color: '#000000',
-        fontSize: 16,
-        fontFamily: 'Poppins_400Regular',
-    },
-    item: {
-        marginTop: 20,
-        flexDirection: 'column',
-    }
-})
+        container: {
+            display: 'flex',
+            backgroundColor: '#e6e8e3',
+            flexDirection: 'row',
+            justifyContent: 'space-around',   
+            paddingStart: 18,
+            paddingTOP: 20,
+            paddingEnd: 28,
+            marginTop: -24,
+            marginStart: 8,
+            marginEnd: 8,
+            borderRadius: 8,
+            paddingBottom:  40,
+            zIndex: 99,
+        },
+        item: {
+            display: 'flex',
+            marginTop: 20,
+            flexDirection: 'column',
+            
+        },
+        itemGasto: {
+            color: '#ff0000',
+            fontSize: 26,
+            fontWeight: 'bold',
+        },
+        itemSaldo: {
+            color: '#00ff00',
+            fontSize: 26,
+            fontWeight: 'bold',
+        },
+        content: {
+            flexDirection: 'row',
+        },
+        currencySymbol: {
+            color: '#000000',
+            marginRight: 6,
+            marginStart: 6,
+        },
+        balance: {
+            color: '#000000',
+            fontSize: 16,
+            fontWeight: 'bold',
+        },
+});
+    
